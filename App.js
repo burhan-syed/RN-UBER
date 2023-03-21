ca26b0587ae85447405ff52f1ae7ee2e4b14715c
Burhan Syed <burhanudinsyed@gmail.com>
2023-03-20 20:31:05 -0500

    basic navoptions, rneui

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={tw`text-red-500`}>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <NavOptions />
+      </View>
+      <Text>HomeScreen</Text>
     </SafeAreaView>
   );
 };
