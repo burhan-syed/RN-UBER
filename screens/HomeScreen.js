48e3ae48207675cf9a498dfabc4a7c37cadf4754
Burhan Syed <burhanudinsyed@gmail.com>
2023-03-20 20:13:35 -0500

    init tailwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, SafeAreaView } from "react-native";
 import React from "react";
-
+import tw from "twrnc";
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
+      <Text style={tw`text-red-500`}>HomeScreen</Text>
     </SafeAreaView>
   );
 };
